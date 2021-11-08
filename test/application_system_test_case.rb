bd3b8dd000be4be14a20f32dc4d8fd1f36ee6093
Leopold Huelst <leopold.huelst@gmail.com>
2021-11-08 12:26:44 +0100

    done

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,3 +3,7 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
+
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  driven_by :headless_chrome # Update this line
+end
